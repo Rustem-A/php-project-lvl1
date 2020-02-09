26ccf43ec570460c85c92b7d6e4ab5952664d856
Rustem-A <Rustem-A@mail.ru>
2020-02-09 14:58:16 +0300

    second game completed

---
+++ src/games/Calc.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BrainGames\games\Calc;
+
+use function BrainGames\Cli\run;
+
+const DESCRIPTION = 'What is the result of the expression?';
+
+function play()
+{
+    $foo = function () {
+        $num = rand(1, 10);
+        $num2 = rand(1, 10);
+        $operands = ['+', '-', '*'];
+        $operand = $operands[rand(0, 2)];
+
+        switch ($operand) {
+        case "-":
+            $result = $num - $num2;
+            break;
+        case "+":
+            $result = $num + $num2;
+            break;
+        case "*":
+            $result = $num * $num2;
+            break;
+        default:
+            return false;
+        }
+        
+        $question = "$num$operand$num2";
+        $arr[] = $question;
+        $arr[] = $result;
+        return $arr;
+    };
+    run(DESCRIPTION, $foo);
+}
